228499bc0b51bee03c7926c369c89b35510d9ff3
harithas1 <harithaharitha0862s@gmail.com>
2025-04-04 22:25:47 +0530

    added config file

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+const CONFIG = {
+  API_KEY: "5b15a021068a09edff8b03e2ea1d9cf9",
+  DEFAULT_CITY: "Chittoor",
+};
--- wmain.js
+++ wmain.js
@@ -1,4 +1,4 @@
-const API_KEY = import.meta.env.VITE_API_KEY;
+const API_KEY = CONFIG.API_KEY;
 
 const Day_Of_The_Week = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const loadCurrentForecast = async ({ lat, lon }) => {
     //1
 
-    const cityName = import.meta.env.VITE_DEFAULT_CITY || "Chittoor";
+    const cityName = CONFIG.DEFAULT_CITY || "Chittoor";
     const response =
       lat && lon
         ? await fetch(
